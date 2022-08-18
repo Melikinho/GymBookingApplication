6ec9e550a14091ca34d259205f45a23d4ed5266b
Melikinho <Jimmymelik10@gmail.com>
2022-08-18 16:54:01 +0200

    Update again property

--- GymBookingApplication/Models/ApplicationUserGymClass.cs
+++ GymBookingApplication/Models/ApplicationUserGymClass.cs
@@ -1,7 +1,9 @@
 ﻿namespace GymBookingApplication.Models
 {
     public class ApplicationUserGymClass
+#nullable disable
     {
+        public int Id { get; set; }
         public int GymClassId { get; set; } 
         public GymClass GymClass { get; set; }
 
