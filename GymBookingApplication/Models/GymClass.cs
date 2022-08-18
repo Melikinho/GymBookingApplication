345ba4ee25add57872a5767ee62ff5ea79e97f86
Melikinho <Jimmymelik10@gmail.com>
2022-08-18 14:28:17 +0200

    Adding GymClass in Models with Properties

---
+++ GymBookingApplication/Models/GymClass.cs
@@ -0,0 +1,14 @@
+﻿namespace GymBookingApplication.Models
+{
+
+    public class GymClass
+#nullable disable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime { get { return StartTime + Duration; } }     /// Observera ; efter 'Duration'
+        public String Description { get; set; }
+    }
+}
