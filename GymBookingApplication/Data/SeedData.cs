1374003ecb854f412231ea66b3ea0c23c6380fa5
Melikinho <Jimmymelik10@gmail.com>
2022-08-25 15:57:46 +0200

    Adding Admin user in Seed

--- GymBookingApplication/Data/SeedData.cs
+++ GymBookingApplication/Data/SeedData.cs
@@ -6,7 +6,23 @@ namespace GymBookingApplication.Data
 {
     public class SeedData
     {
-    }
+        public static async Task InitAsync(ApplicationDbContext db, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, string aPassword)
+        {
 
-            
+            await roleManager.CreateAsync(new IdentityRole( { Name = "Admin" }));
+
+            {
+                var adminUser = new ApplicationUser()
+                {
+                    Email = "admin@jimmysgym.com",
+                    UserName = "admin@jimmysgym.com",
+                    FirstName = "Admin",
+                    LastName = "Admins",
+                    EmailConfirmed = true
+                };
+            }
+        }
+
+
+    }
 }
